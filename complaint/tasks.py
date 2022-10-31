2308256b1c74371b7dc46c31faaf23aaacae4bfb
Tushar-affinsys <115154882+Tushar-affinsys@users.noreply.github.com>
2022-10-31 12:28:22 +0530

    Update tasks.py

--- complaint/tasks.py
+++ complaint/tasks.py
@@ -1,17 +1,17 @@
-# from time import sleep
-# from celery import shared_task
-# from django.core.mail import send_mail
-#
-#
-# @shared_task()
-# def send_feedback_email_task(message):
-#     email_from = settings.EMAIL_HOST_USER
-#     recipient = ['tusharsingla2822@gmail.com', ]
-#     sleep(20)
-#     send_mail(
-#         "Your Feedback",
-#         f"\t{message}\n\nThank you!",
-#         email_from,
-#         recipient,
-#         fail_silently=False,
-#     )
+from time import sleep
+from celery import shared_task
+from django.core.mail import send_mail
+
+
+@shared_task()
+def send_feedback_email_task(message):
+    email_from = settings.EMAIL_HOST_USER
+    recipient = ['tusharsingla2822@gmail.com', ]
+    sleep(20)
+    send_mail(
+        "Your Feedback",
+        f"\t{message}\n\nThank you!",
+        email_from,
+        recipient,
+        fail_silently=False,
+    )
